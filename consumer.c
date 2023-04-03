5c3db4715a3d3fe29b5c71f9315fcabb8350935d
Stillemere <hellotoad3@gmail.com>
2023-04-03 02:13:20 -0400

    some progress although still broken

--- consumer.c
+++ consumer.c
@@ -5,26 +5,37 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
+#include <iostream>
+using std::cerr;
+
 #define TABLE_SIZE 2
 
+
 int table[TABLE_SIZE];
-int index = 0;
+int i = 0;
 
 sem_t *empty;
 sem_t *full;
+
 pthread_mutex_t mutex;
 
 void *consumer(void *arg) {
+    cerr << "c: consumer invoked!\n";
     int item = 0;
     while (1) {
+	    cerr << "c: waiting for full slot.\n";
         sem_wait(full); // wait for full slot
+	    cerr << "c: entering critical section\n";
         pthread_mutex_lock(&mutex); // enter critical section
-        item = table[index-1]; // get item from table
+	    cerr << "c: getting item from table\n";
+        item = table[i-1]; // get item from table
         printf("Consumer: Consumed item %d\n", item);
-        index--;
+        i--;
         pthread_mutex_unlock(&mutex); // exit critical section
+            cerr << "c: exited critical section.\n";
         sem_post(empty); // signal that table is empty
-        if (index == 0) {
+	    cerr << "c: table is empty.";
+        if (i == 0) {
             printf("Table is empty. Consumer is waiting.\n");
         }
     }
@@ -32,8 +43,11 @@ void *consumer(void *arg) {
 }
 
 int main() {
+    cerr << "c:(main) entered main.\n";
     empty = sem_open("empty", O_CREAT, 0644, TABLE_SIZE);
     full = sem_open("full", O_CREAT, 0644, 0);
+    //sem_init(&sem, 0, TABLE_SIZE);
+
     pthread_mutex_init(&mutex, NULL);
 
     pthread_t cons_tid;
@@ -41,13 +55,17 @@ int main() {
 
     pthread_join(cons_tid, NULL);
 
+    /*
+    sem_close(&sem);
+    sem_unlink("sem");
+    */
     sem_close(empty);
     sem_close(full);
     sem_unlink("empty");
     sem_unlink("full");
-
+    
     pthread_mutex_destroy(&mutex);
-
+    cerr << "c:(main) end of main.\n";
     return 0;
 }
 
--- producer.c
+++ producer.c
@@ -5,49 +5,70 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
+#include <iostream>
+using std::cerr;
+
 #define TABLE_SIZE 2
 
 int table[TABLE_SIZE];
-int index = 0;
+int i = 0;
 
 sem_t *empty;
 sem_t *full;
+//sem_t sem;
+
 pthread_mutex_t mutex;
 
 void *producer(void *arg) {
+    cerr << "p: Producer invoked!\n";
     int item = 0;
     while (1) {
         item = rand() % 100; // generate item
+	cerr << "p: item generated. waiting for empty slot.\n";
         sem_wait(empty); // wait for empty slot
+		cerr << "p: empty slot available.\n";
         pthread_mutex_lock(&mutex); // enter critical section
-        table[index] = item; // put item on table
-        printf("Producer: Produced item %d\n", item);
-        index++;
+		cerr << "p: entering critical section.\n";
+        table[i] = item; // put item on table
+		cerr << "p: item placed on table.\n";
+        printf("p: Producer: Produced item %d\n", item);
+        i++;
         pthread_mutex_unlock(&mutex); // exit critical section
+		cerr << "p: exiting critical section.\n";
         sem_post(full); // signal that table is full
-        if (index == TABLE_SIZE) {
-            printf("Table is full. Producer is waiting.\n");
+		cerr << "p: table is full.\n";
+        if (i == TABLE_SIZE) {
+            printf("p: Table is full. Producer is waiting.\n");
         }
     }
     pthread_exit(NULL);
 }
 
 int main() {
+    cerr << "\np:(main) producer.c now running\n";
+
     empty = sem_open("empty", O_CREAT, 0644, TABLE_SIZE);
     full = sem_open("full", O_CREAT, 0644, 0);
+    //sem_init(&sem, 0, TABLE_SIZE);
+
     pthread_mutex_init(&mutex, NULL);
 
     pthread_t prod_tid;
-    pthread_create(&prod_tid, NULL, producer, NULL);
 
-    pthread_join(prod_tid, NULL);
+    pthread_create(&prod_tid, NULL, producer, NULL); cerr << "p:(main) producer thread created.\n";
 
-    sem_close(empty);
-    sem_close(full);
-    sem_unlink("empty");
-    sem_unlink("full");
 
-    pthread_mutex_destroy(&mutex);
+    pthread_join(prod_tid, NULL);   // wait for producer thread to exit
+    cerr << "p:(main) producer thread exited";
+
+    sem_close(empty); cerr << "p:(main) semiphore close empty";
+    sem_close(full);	cerr << "p:(main) semiphore close full";
+    sem_unlink("empty");	cerr << "p:(main) semiphore unlink empty";
+    sem_unlink("full");		cerr << "p:(main) semiphore unlink full";
+
+    pthread_mutex_destroy(&mutex);	cerr << "p:(main) mutex destroy";
+    
+    printf("p:(main) completed main.\n");
 
     return 0;
 }
